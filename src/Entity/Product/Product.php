a191682470545ed4eed71a64b3d1bf31b6251420
tiboCorb <corbrionthibault@gmail.com>
2019-06-23 23:46:21 +0200

    main commit

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping\MappedSuperclass;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Table;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
 
 /**
- * @MappedSuperclass
- * @Table(name="sylius_product")
+ * @ORM\Entity()
+ * @ORM\Table(name="sylius_product")
  */
 class Product extends BaseProduct
 {
+   
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
